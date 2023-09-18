d1822a2fdf7c682a6a9e82b078e6505bfd4dfbc2
Melvin Wahlberg <mellepella@gmail.com>
2023-09-18 10:25:57 +0200

    Polish luhn_algorithm function

--- luhn_algorithm.rb
+++ luhn_algorithm.rb
@@ -4,14 +4,15 @@ def luhn_algorithm numbers
   sum = 0
 
   numbers.each_char.with_index do |char, index|
-    if char.numeric?
-      if index.even?
-        multiplied_value = Integer(char) * 2;
-        separated_values = multiplied_value.to_s.scan(/./)
-        separated_values.each {|num| sum += Integer(num)}
-      else 
-        sum += Integer(char);
-      end
+    char_is_not_numeric = !char.numeric?
+    next if char_is_not_numeric
+
+    if index.even?
+      multiplied_value = char.to_i * 2;
+      separated_values = multiplied_value.to_s.scan(/./)
+      separated_values.each {|num| sum += num.to_i}
+    else
+      sum += char.to_i;
     end
   end
 
